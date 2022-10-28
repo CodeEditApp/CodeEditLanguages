59c3c56a8dea4290101bad27c162083681377cc7
Lukas Pistrol <lukas@pistrol.com>
2022-10-28 23:47:43 +0200

    add swift docc plugin

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
     ],
     dependencies: [
         .package(
+            url: "https://github.com/apple/swift-docc-plugin.git",
+            from: "1.0.0"
+        ),
+        .package(
             url: "https://github.com/ChimeHQ/SwiftTreeSitter.git",
             exact: "0.7.0"
         ),
