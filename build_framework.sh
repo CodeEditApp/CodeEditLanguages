402f0b73d3c6de58181bdef5cc416e6c0fa494a0
Lukas Pistrol <lukas@pistrol.com>
2022-11-05 18:46:31 +0100

    update build script
    implement green status messages.
    by default hide any xcodebuild output.
    use '--debug' to display verbose output.

--- build_framework.sh
+++ build_framework.sh
@@ -1,15 +1,44 @@
 #!/usr/bin/env sh
 
+# This script builds the CodeLanguagesContainer.xcframework
+#
+# Just call it from the root of the project
+# $ ./build_framework.sh
+#
+# If you need debug output, set the --debug flag
+# $ ./build_framework.sh --debug
+#
+# Created by: Lukas Pistrol on 29.10.2022
+
+# convenience function to print a status message in green
+status () {
+    local GREEN='\033[0;32m'
+    local NC='\033[0m' # No Color 
+    echo "${GREEN}◆ $1${NC}"
+}
+
+# If --debug set -quiet flag and redirect output to /dev/null
+if [ "$1" = "--debug" ]; then
+    QUIET_FLAG=""
+    QUIET_OUTPUT=/dev/stdout
+else
+    QUIET_FLAG="-quiet"
+    QUIET_OUTPUT=/dev/null
+fi
+
+# Set pipefail to make sure that the script fails if any of the commands fail
 set -euo pipefail
 
 # build the framework project `CodeLanguages-Container`
+status "Clean Building CodeLanguages-Container.xcodeproj..."
 xcodebuild \
     -project CodeLanguages-Container/CodeLanguages-Container.xcodeproj \
     -scheme CodeLanguages-Container \
     -destination "platform=macOS" \
     -derivedDataPath DerivedData \
     -configuration Release \
-    -quiet clean build
+    $QUIET_FLAG clean build &> $QUIET_OUTPUT
+status "Build complete!"
 
 # set path variables
 PRODUCTS_PATH="$PWD/DerivedData/Build/Products/Release"
@@ -19,18 +48,23 @@ OUTPUT_PATH="CodeLanguagesContainer.xcframework"
 # remove previous generated files
 rm -rf "$OUTPUT_PATH"
 rm "$OUTPUT_PATH".zip
+status "Removed previous generated files!"
 
 # build the binary framework
+status "Creating CodeLanguagesContainer.xcframework..."
 xcodebuild \
     -create-xcframework \
     -framework "$FRAMEWORK_PATH" \
-    -output "$OUTPUT_PATH"
+    -output "$OUTPUT_PATH" &> $QUIET_OUTPUT
 
 # zip the xcframework
+status "Zipping CodeLanguagesContainer.xcframework..."
 zip -r -q "$OUTPUT_PATH".zip "$OUTPUT_PATH"
 
 # remove the unzipped xcframework
 rm -rf "$OUTPUT_PATH"
+
+status "CodeLanguagesContainer.xcframework.zip created!"
 
 # copy language queries to package resources
 # set path variables
@@ -38,6 +72,7 @@ CHECKOUTS_PATH="$PWD/DerivedData/SourcePackages/checkouts"
 RESOURCES_PATH="$PWD/Sources/CodeEditLanguages/Resources"
 
 # remove previous copied files
+status "Copying language queries to package resources..."
 rm -rf "$RESOURCES_PATH"
 
 # find and copy language queries 
@@ -45,7 +80,6 @@ LIST=$( echo $CHECKOUTS_PATH/tree-* )
 
 for lang in $LIST ; do
     name=${lang##*/}
-    echo "Copying queries: $name"
     mkdir -p $RESOURCES_PATH/$name
     highlights=$( find $lang -type f -name "*.scm" )
     for highlight in $highlights ; do
@@ -53,5 +87,6 @@ for lang in $LIST ; do
         cp $highlight $RESOURCES_PATH/$name/$highlight_name
     done
 done
+status "Language queries copied to package resources!"
 
-echo "Done!"
+status "Done!"
